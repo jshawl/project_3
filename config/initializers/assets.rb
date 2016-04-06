35d8fb27446308c5765ac5632ab24119e5787a9e
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-04-06 19:14:14 -0400

    Homepage styled

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( trips.js )
+Rails.application.config.assets.precompile += %w( trips.js application.css )
